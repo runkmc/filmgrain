0d8714f35949d213c6c690bbf55133a147d64b8a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-03-20 13:56:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Filmgrain::Application.config.secret_key_base = 'abde1e46090a7147bcfda27feaebc4aa2fef45696cd2cd23d5e9ce3a2e477e1866c51379ea68813fbbbffa713423c2f1874813e3e7607b67e4672209b1ebba1a'
